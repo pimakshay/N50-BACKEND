6d9708c069e211dcb708ddcac1b8c3eb759307d5
akshayp <akshay.pimpalkar@tum.de>
2022-09-06 17:12:49 +0200

    initial commit

---
+++ src/data_preprocessor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+
+class DataHandler:
+
+    def __init__(self) -> None:
+        
+        pass
+
+    def read_csv(col_names, filenames):
+        '''
+            This function reads all the text files and stores the data in dataframes.
+            Args:
+            :col_names: column names of the individual data files
+            :filenames: name of files (companies) for the given data
+        '''
+        main_df = pd.DataFrame(columns=col_names)
+        # l = [pd.read_csv(filename, names=col_names) for filename in glob.glob("data/nifty50/05SEPsample/*.txt")
+        #         if filename[0]]
+        # df = pd.concat(l, axis=0)
+        for filename in filenames:
+                df = pd.read_csv(filename, names=col_names)
+                basename = os.path.basename(filename) #gives companyname.txt
+                bs1,bs2 = basename.split('.') # splits the string
+
+                # rename close and volume columns; and remove other columns from df
+                df.rename(columns={"close":f"{bs1}_close", "volume":f"{bs1}_volume"}, inplace=True)
+                df.set_index("time",inplace=True)
+                df = df[[f"{bs1}_close",f"{bs1}_volume"]]
+
+                if len(main_df) == 0:
+                        main_df = df
+                else:
+                        main_df = main_df.join(df)
+
+        return main_df
